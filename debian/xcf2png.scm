c26ac63209e04c9a26d096a70839fd7f7d75d510
Balint Reczey <balint@balintreczey.hu>
2015-01-02 15:00:18 +0100

    Generate "from Debian" logo from SVG

--- debian/xcf2png.scm
+++
@@ -1,8 +0,0 @@
-; based on http://stackoverflow.com/questions/5794640/how-to-convert-xcf-to-png-using-gimp-from-the-command-line
-(define (xcf2png from to)
-  (let* ((image 0) (layer 0))
-    (set! image (car (gimp-file-load RUN-NONINTERACTIVE from from)))
-    (set! layer (car (gimp-image-merge-visible-layers image CLIP-TO-IMAGE)))
-    (gimp-file-save RUN-NONINTERACTIVE image layer to to)
-    )
-  )
